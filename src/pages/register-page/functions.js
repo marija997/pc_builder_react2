d3bad8b883d8f8f7b6e37dd08a58fad5dff38e48
Smuchy <marija.stankovic@jolausa.com>
2020-05-23 21:21:49 +0200

    Post request for user register

--- src/pages/register-page/functions.js
+++ src/pages/register-page/functions.js
@@ -2,6 +2,7 @@ export const RegisterSubmit = (input) => {
   fetch("http://localhost:5000/register", {
     method: "POST",
     headers: {
+      Accept: "application/json",
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
